f8e995b1a4dbd93d20211551fd30fc4974038a39
genekomarov <gene.komarov@gmail.com>
2020-05-12 20:20:27 +0500

    Updates

---
+++ precache-manifest.2da79810fe443de437275a61fcb8206f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8d04ecd9400876fb4c359f9cb19b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5b13f67d3c3ee55246",
+    "url": "/static/css/main.70fbcc81.chunk.css"
+  },
+  {
+    "revision": "ebcfaf271ed7adaf8840",
+    "url": "/static/js/2.60bf60bc.chunk.js"
+  },
+  {
+    "revision": "bceb19a85aa88956d6da0c5436b1193c",
+    "url": "/static/js/2.60bf60bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5b13f67d3c3ee55246",
+    "url": "/static/js/main.c2ac6ff7.chunk.js"
+  },
+  {
+    "revision": "8a1351d2d24d597e63c2",
+    "url": "/static/js/runtime-main.3bd924fb.js"
+  },
+  {
+    "revision": "a860bea40d0aa2cbdb2645af9819c894",
+    "url": "/static/media/cards.a860bea4.jpg"
+  }
+]);
